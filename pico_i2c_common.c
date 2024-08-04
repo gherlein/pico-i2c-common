4ce412bb676858ab6e22bd4fe595d79b70fb3683
Greg Herlein <gherlein@herlein.com>
2024-08-04 14:30:41 -0700

    finally working as a lib

---
+++ pico_i2c_common.c
@@ -0,0 +1,71 @@
+/*
+    This code originates from the Getting started with Raspberry Pi Pico
+   document
+    https://datasheets.raspberrypi.org/pico/getting-started-with-pico.pdf
+    CC BY-ND Raspberry Pi (Trading) Ltd
+*/
+
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
+#include "pico/binary_info.h"
+#include "pico/stdlib.h"
+#include "pico/time.h"
+#include "pico/types.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include "pico_i2c_common.h"
+
+
+static bool reserved_addr(uint8_t addr) {
+  return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+}
+
+void scan_i2c(i2c_inst_t *i2c) {
+  printf("\nI2C Bus Scan\n");
+  printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+
+  for (int addr = 0; addr < (1 << 7); ++addr) {
+    if (addr % 16 == 0) {
+      printf("%02x ", addr);
+    }
+
+    // Perform a 1-byte dummy read from the probe address. If a slave
+    // acknowledges this address, the function returns the number of bytes
+    // transferred. If the address byte is ignored, the function returns
+    // -1.
+
+    // Skip over any reserved addresses.
+    int ret;
+    uint8_t rxdata;
+    if (reserved_addr(addr))
+      ret = PICO_ERROR_GENERIC;
+    else
+      ret = i2c_read_blocking(i2c, addr, &rxdata, 1, false);
+
+    printf(ret < 0 ? "." : "@");
+    printf(addr % 16 == 15 ? "\n" : "  ");
+  }
+  printf("Done.\n");
+}
+
+#ifdef MAIN
+const uint LED_PIN = 25;
+
+int main() {
+  bi_decl(bi_program_description("PROJECT DESCRIPTION"));
+
+  stdio_init_all();
+
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  while (1) {
+    gpio_put(LED_PIN, 0);
+    sleep_ms(250);
+    gpio_put(LED_PIN, 1);
+    puts("Hello World\n");
+    sleep_ms(1000);
+  }
+}
+#endif
---
+++ pico_i2c_common.h
@@ -0,0 +1,1 @@
+void scan_i2c(i2c_inst_t *i2c);
